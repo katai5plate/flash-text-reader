f5dce1c4694fa74257676ccf72ca2f792cc92ebb
katai5plate <katai5plate.sub@gmail.com>
2020-09-18 00:35:58 +0900

    init

---
+++ index.js
@@ -0,0 +1,73 @@
+const LIMIT = 4;
+
+window.defaultText = "";
+window.tokenizer = null;
+window.isProcessing = false;
+const button = document.getElementById("submit");
+const input = document.getElementById("text");
+
+const changeButtonText = (text) => (button.innerText = text);
+
+const step = async (list, cb, ms) => {
+  let res = [];
+  let index = 0;
+  for (const elm of list) {
+    index++;
+    res.push(
+      await new Promise((r) => {
+        setTimeout(async () => {
+          r(await cb(elm, index, list));
+        }, ms);
+      })
+    );
+  }
+  return res;
+};
+
+changeButtonText("読み込み中...");
+fetch("./README.md")
+  .then((res) => res.text())
+  .then((res) => {
+    window.defaultText = res;
+    kuromoji.builder({ dicPath: "./dict" }).build((e, k) => {
+      if (e) throw e;
+      window.tokenizer = k;
+      button.disabled = false;
+      changeButtonText("[[ 決定 ]]");
+    });
+  });
+
+const tokenize = (text) =>
+  window.tokenizer &&
+  window.tokenizer
+    .tokenize(text.replace(/\s+/g, ""))
+    .map(({ surface_form }) => surface_form)
+    .reduce(
+      ({ stock, result }, c) =>
+        stock.length + c.length >= LIMIT
+          ? { stock: "", result: [...result, stock + c] }
+          : { stock: stock + c, result },
+      {
+        stock: "",
+        result: [],
+      }
+    ).result;
+
+button.onclick = () => {
+  if (window.isProcessing) return;
+  changeButtonText("");
+  window.isProcessing = true;
+  const { value } = input;
+  const result = tokenize(value || window.defaultText);
+  step([...result, ""], (v) => changeButtonText(v), 500)
+    .then(() => {
+      window.isProcessing = false;
+      console.log({ result });
+      changeButtonText("[[ 決定 ]]");
+    })
+    .catch((e) => {
+      window.isProcessing = false;
+      changeButtonText("エラーが発生しました");
+      throw e;
+    });
+};
